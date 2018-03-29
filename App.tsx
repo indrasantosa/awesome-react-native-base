7a9612c1864c0e97f1c891e243430bbf79222353
Indra Santosa <indrasantosa90@gmail.com>
2018-03-30 02:47:49 +0800

    add react navigation

--- App.tsx
+++ App.tsx
@@ -2,6 +2,7 @@ import * as React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import { Container, Content, Header, Title, Button, Left, Right, Body, Icon } from 'native-base'
 import { Font } from 'expo'
+import { StackNavigator } from 'react-navigation'
 
 export default class App extends React.Component<{}> {
 
